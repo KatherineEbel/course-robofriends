32f1a783fc5e322ffefd2f07ff8b6a335152065d
kathyebel <kathyebel@me.com>
2018-09-19 06:44:05 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+registerServiceWorker();
